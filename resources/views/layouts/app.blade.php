8f5af4fe12d53ea8e63c253fdc98afa52d202591
favour.obasi <favour.obasi@abujaelectricity.com>
2025-03-16 19:30:58 +0100

    update css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <!-- Bootstrap CSS -->
     <link href="{{asset('dashboard/assets/css/bootstrap.min.css')}}" rel="stylesheet" />
     <link href="{{asset('dashboard/assets/css/bootstrap-extended.css')}}" rel="stylesheet" />
-    <link href="{{asset('dashboard/assets/css/style.css')}}" rel="stylesheet" />
+    <link href="{{asset('dashboard/assets/css/style.css?v=1.0')}}" rel="stylesheet" />
     <link href="{{asset('dashboard/assets/css/icons.css')}}" rel="stylesheet" />
     {{-- <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet"> --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
